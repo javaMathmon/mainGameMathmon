696cbcf41a9f4fad28bf78738c0eea3117bfcd66
VeraAguila <vaguila.feucsodataanalytics@gmail.com>
2024-12-06 22:18:14 +0800

    clean folder

---
+++ mathmonGame/src/main/mathmonHealth.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class mathmonHealth extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public mathmonHealth() {
+		setSize(540,150);
+	}
+	
+//	public void drawCharacterScreen() {
+//		final int frameX = gp.tileSize*2;
+//		final int frameY = gp.tileSize;
+//		final int frameWidth = gp.tileSize*5;
+//		final int frameHeight = gp.tileSize*10;
+//		drawSubWindow(frameX, frameY, frameWidth, frameHeight);
+//		
+//		
+//	}
+//
+//	public void drawSubWindow(int x, int y, int width, int height) {
+//		Color c = new Color(0,0,0,210);
+//		g2.setColor(c);
+//		g2.fillRoundRect(x,y,width,height,35,35);
+//		
+//		c = new Color(255,255,255);
+//		g2.setColor(c);
+//		g2.setStroke(new BasicStroke(5));
+//		g2.drawRoundRect(x+5,y+5,width-10,height-10,25,25);
+//	}
+}
